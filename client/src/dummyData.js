699285f14f66211886423af325b31ca7a9817a8b
Brock Ross <m.brock.ross@gmail.com>
2019-06-06 12:39:39 -0600

    add dummy json files

---
+++ client/src/dummyData.js
@@ -0,0 +1,16 @@
+const sampleApplication = {
+  name: "Bandland",
+  jobTitle: "software engineer",
+  techs: [
+    "PostgreSQL",
+    "Node",
+    "AWS"
+  ],
+  description: "Legacy back-end codebase of app used for streaming albums, along with displaying album art, artist info, comments, and recommendations",
+  bullets: [
+    "Scaled a legacy database and Express API to handle web scale traffic (~10M database entries; 100 RPS)",
+    "Improved request latency by 100% by load balancing with nginx",
+    "Optimized static asset compression and delivery, taking Google PageSpeed score from 6 to 100",
+    "Utilized agile workflow to complete individual and team deliverables, meeting or exceeding deadlines and quality standards"
+  ]
+}

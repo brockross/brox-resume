565b931deefe94d8aff644f5befbc9c850f12e6f
Brock Ross <m.brock.ross@gmail.com>
2019-06-06 13:04:33 -0600

    pass dummy data in SWEApps test (now passing)

--- client/src/components/tests/SweApplications.test.js
+++ client/src/components/tests/SweApplications.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import SweApplications from  '../SweApplications.jsx';
+import sampleData from '../../dummyData.js';
 
 describe('SweApplications', () => {
   it('should render correctly', () => {
-    const component = shallow(<SweApplications/>);
+    const component = shallow(<SweApplications sweApps={JSON.parse(sampleData)}/>);
 
     expect(component).toMatchSnapshot();
   });
